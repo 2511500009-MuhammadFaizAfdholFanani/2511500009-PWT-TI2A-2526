dc7be9bc06ccb22373a2d127d9a042685a3cc230
Muhammad Faiz Afdhol Fanani <2511500009@mahasiswa.atmaluhur.ac.id>
2026-03-27 21:06:56 +0700

    mengubah Beberapa Tampilan dasboard Per-Role Masing masing

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
-          <li class="nav-item menu-open">
+          <li class="nav-item">
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>Master<i class="right fas fa-angle-left"></i></p>
